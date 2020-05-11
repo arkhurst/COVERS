d6d01497a9b6a5ad020a27d7a76fe601e7fb5008
unknown <fiiarkhurst@gmail.com>
2020-05-11 19:19:51 +0100

    finished profile  modal

--- components/header/profileModal.js
+++ components/header/profileModal.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { View, Text, Modal, StyleSheet, Keyboard } from 'react-native';
 import Constants from 'expo-constants';
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons, AntDesign } from '@expo/vector-icons';
 import { TouchableOpacity, TextInput } from 'react-native-gesture-handler';
+import RadioButtonRN from 'radio-buttons-react-native';
 
 export default function ProfileModal({visible, cancel}){
+
+    const data = [
+        {
+        label:'Male'
+        },
+        {
+        label:'Female'
+       }
+    ]
 
     // hide keypad on iOS
     // function hide(){
@@ -23,9 +33,55 @@ export default function ProfileModal({visible, cancel}){
                     <Text style={styles.mainTitle}>Personal Details</Text>
                 {/* Age Details */}
                     <View style={styles.detailsItem}>
-                      <Text>Enter Age</Text>
+                      <Text style={styles.mainText}>Enter Age</Text>
                       <TextInput keyboardType="number-pad" style={styles.textInput}>30</TextInput>
                     </View>
+                    {/* Radio buttons */}
+                    <View>
+                        <RadioButtonRN 
+                          data={data}
+                          animationTypes={['shake']}
+                          circleSize={16}
+                          initial={3}
+                          box={false}
+                          activeColor="black"
+                          inactiveColor="grey"
+                          textStyle={{ fontFamily:'AirbnbCereal-Bold',letterSpacing:-0.4,}}
+                          icon={
+                              <AntDesign 
+                                name="checkcircle"
+                                size={20}
+                                // color="#2c9dd1"
+                              />
+                          }
+                        />
+                    </View>
+                    {/* Travel History */}
+                    <View style={styles.detailsItem}>
+                          <Text style={styles.mainTitle}>Travel History</Text>
+                          <Text style={styles.mainText}>Select the last two countries you visited (If Applicable)</Text>
+                          <View style={styles.countryContainer}>
+                             <TouchableOpacity style={styles.selectCountry}>
+
+                             </TouchableOpacity>
+                             <TouchableOpacity style={styles.selectCountry}>
+
+                             </TouchableOpacity>
+                          </View>
+                    </View>
+                    {/* Medical professional */}
+                    <View>
+                       <Text style={styles.mainTitle}>Medical Professional Information</Text>
+                       <Text style={styles.mainText}>Applicable if you are a health worker</Text>
+                       <View style={styles.detailsItem}>
+                            <Text style={styles.mainText}>Health License Number</Text>
+                            <TextInput style={styles.textInput} keyboardType="number-pad" />
+                       </View>
+                    </View>
+                    {/* Button */}
+                    <TouchableOpacity style={styles.button}>
+                          <Text style={[styles.mainText],{color:'white'}}>Update Profile</Text>
+                    </TouchableOpacity>
                 </View>
             </View>
         </Modal>
@@ -54,14 +110,38 @@ const styles = StyleSheet.create({
         fontFamily:'AirbnbCereal-Bold',
         letterSpacing:-0.2,
     },
+    mainText:{
+        fontFamily:'AirbnbCereal-Bold',
+        letterSpacing:-0.2,
+        color:'grey'
+    },
     detailsItem:{
         paddingTop:20
     },
     textInput:{
         borderWidth:StyleSheet.hairlineWidth,
         height:50,
-        marginVertical:20,
+        marginVertical:10,
         borderColor:'grey',
         paddingHorizontal:20
+    },
+    countryContainer:{
+       flexDirection:'row',
+       marginVertical:30,
+       justifyContent:'center'   
+    },
+    selectCountry:{
+        width:180,
+        height:120,
+        borderRadius:10,
+        borderWidth:1.5,
+        marginHorizontal:5,
+        borderColor:'grey'
+    },
+    button:{
+        backgroundColor:'black',
+        justifyContent:'center',
+        alignItems:'center',
+        height:52
     }
 })
