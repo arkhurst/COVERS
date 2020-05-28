61db5d68eb293e53933d69f46cf36290ee853402
unknown <fiiarkhurst@gmail.com>
2020-05-28 21:27:28 +0100

    🎈

--- screens/settings/modals/testingCenter.js
+++ screens/settings/modals/testingCenter.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet, Modal} from 'react-native';
+import { View, Text, StyleSheet, Modal} from 'react-native';
 import Constants from 'expo-constants';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { Ionicons } from '@expo/vector-icons';
 
 
 export default function TestinCenters({close, visible}){
     return(
         <Modal visible={visible} presentationStyle={'pageSheet'} animationType={'slide'} >
             <View style={styles.container}>
-            <Button title="close" onPress={() => close(3)} />
-            <Text>Testing Center</Text>
+                <View style={styles.header}>
+                    <Text style={styles.headerTitle}>Testing Centers</Text>
+                    <TouchableOpacity onPress={() => close(3)}>
+                        <Ionicons  name="ios-close" size={40} />
+                    </TouchableOpacity>
+                </View>
             </View>
-
         </Modal>
     )
 }
@@ -18,6 +23,17 @@ export default function TestinCenters({close, visible}){
 const styles = StyleSheet.create({
     container:{
       flex:1,
-      paddingTop:Constants.statusBarHeight
-    }
+      paddingTop:Constants.statusBarHeight,
+      paddingHorizontal:20
+    },
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginVertical: 20,
+      },
+      headerTitle: {
+        fontFamily: 'AirbnbCereal-Bold',
+        letterSpacing: -0.2,
+        fontSize: 32,
+      },
 })
