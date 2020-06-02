74f68a463c5b4d5e8706a0cb0317a2c93412da2b
unknown <fiiarkhurst@gmail.com>
2020-06-02 17:41:02 +0100

    added testing centers directions

--- screens/settings/modals/testingCenter.js
+++ screens/settings/modals/testingCenter.js
@@ -1,11 +1,48 @@
 import React from 'react';
-import { View, Text, StyleSheet, Modal} from 'react-native';
+import { View, Text, StyleSheet, Modal, Button} from 'react-native';
 import Constants from 'expo-constants';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { Ionicons } from '@expo/vector-icons';
+import getDirections from 'react-native-google-maps-directions';
 
 
 export default function TestinCenters({close, visible}){
+
+    const handleGetDirections = () => {
+        const data = {
+          
+            destination:{
+                latitude:6.673175,
+                longitude:-1.565423
+            },
+            params: [
+                {
+                    key:'travelmode',
+                    value:'driving'
+                },
+                {
+                    key:'dir_action',
+                    value:'navigate'
+                }
+            ],
+            waypoints:[
+                {
+                    latitude:6.673175,
+                    longitude:-1.565423
+                },
+                {
+                    latitude:6.673175,
+                    longitude:-1.565423
+                },
+                {
+                    latitude:6.673175,
+                    longitude:-1.565423
+                }
+            ]
+        }
+
+        getDirections(data)
+    }
     return(
         <Modal visible={visible} presentationStyle={'pageSheet'} animationType={'slide'} >
             <View style={styles.container}>
@@ -15,6 +52,17 @@ export default function TestinCenters({close, visible}){
                         <Ionicons  name="ios-close" size={40} />
                     </TouchableOpacity>
                 </View>
+               <View style={{borderBottomWidth:StyleSheet.hairlineWidth, borderColor:'#dedede'}}>
+                <View style={styles.venueContainer}>
+                    <View>
+                        <Text style={styles.medium}>University of Ghana Medical School</Text>
+                        <Text style={{color:'grey', fontWeight:'600', paddingTop:6}} >Accra, Ghana</Text>
+                    </View>
+                    <TouchableOpacity onPress={handleGetDirections}  >
+                         <Text style={{color:'tomato', fontWeight:'500'}}>Get Directions</Text>
+                    </TouchableOpacity>
+                </View>
+             </View> 
             </View>
         </Modal>
     )
@@ -35,5 +83,15 @@ const styles = StyleSheet.create({
         fontFamily: 'AirbnbCereal-Bold',
         letterSpacing: -0.2,
         fontSize: 32,
+      },
+      venueContainer:{
+          flexDirection:'row',
+          justifyContent:'space-between',
+          paddingVertical:20
+      },
+      medium: {
+        fontFamily: 'AirbnbCereal-Medium',
+        letterSpacing: -0.3,
+        fontSize: 15,
       },
 })
